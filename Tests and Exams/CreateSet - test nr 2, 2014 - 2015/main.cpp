6913527111558e6d354502a38ac79c7ce3284d4c
jakubowiczish <jakub77plotnikowski@gmail.com>
2018-05-05 18:25:01 +0200

    CreateSet small fix

--- Tests and Exams/CreateSet - test nr 2, 2014 - 2015/main.cpp
+++ Tests and Exams/CreateSet - test nr 2, 2014 - 2015/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 /*
- *  Pewna firma przechowuje dużo iczb pierwszych w postaci binarnej jako stringi ”10101...”.
+ *  Pewna firma przechowuje dużo liczb pierwszych w postaci binarnej jako stringi ”10101...”.
     Zaimplementuj strukturę danych Set do przechowywania tych danych. Napisz funkcje:
     Set createSet( string A[], int n ), która tworzy Set z n-elementowej tablicy stringów
     oraz bool contains( Set a, string s ), która sprawdza czy dana liczba jest w Secie.
